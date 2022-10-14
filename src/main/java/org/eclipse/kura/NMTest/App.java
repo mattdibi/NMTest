aea83b29abab55cd56f84482151018e31ca3a9ff
Mattia Dal Ben <matthewdibi@gmail.com>
2022-10-14 16:38:33 +0200

    feat: grab and print address data

--- src/main/java/org/eclipse/kura/NMTest/App.java
+++ src/main/java/org/eclipse/kura/NMTest/App.java
@@ -1,89 +1,43 @@
 package org.eclipse.kura.NMTest;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.freedesktop.dbus.connections.impl.DBusConnection;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.freedesktop.dbus.interfaces.DBusInterface;
-import org.freedesktop.dbus.interfaces.Properties;
-import org.freedesktop.dbus.types.UInt32;
 import org.freedesktop.dbus.types.Variant;
 
-import org.freedesktop.NetworkManager;
 import org.freedesktop.networkmanager.Settings;
-import org.freedesktop.networkmanager.device.Wireless;
 import org.freedesktop.networkmanager.settings.Connection;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) throws DBusException {
         try (DBusConnection dbusConn = DBusConnection.getConnection(DBusConnection.DEFAULT_SYSTEM_BUS_ADDRESS)) {
-            // Get /org/freedesktop/NetworkManager object
-            // See: https://developer-old.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html
-            NetworkManager nm = dbusConn.getRemoteObject("org.freedesktop.NetworkManager",
-                    "/org/freedesktop/NetworkManager", NetworkManager.class);
-
-            System.out.println("------ Permissions -------");
-            Map<CharSequence, CharSequence> getPermissions = nm.GetPermissions();
-            for (Entry<CharSequence, CharSequence> entry : getPermissions.entrySet()) {
-                System.out.println("Permission: " + entry.getKey() + " = " + entry.getValue());
-            }
-
-            // Get Settings object
-            // Get /org/freedesktop/NetworkManager/Settings object
-            // See:
-            // https://developer-old.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.Settings.html
+            // Get /org/freedesktop/NetworkManager/Settings
             Settings settings = dbusConn.getRemoteObject("org.freedesktop.NetworkManager",
                     "/org/freedesktop/NetworkManager/Settings", Settings.class);
 
+            // Iterate over connections
             for (DBusInterface connectionIf : settings.ListConnections()) {
-                // Get Connection object
-                // Get /org/freedesktop/NetworkManager/Settings object
-                // See:
-                // https://developer-old.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.Settings.html
+                // Get /org/freedesktop/NetworkManager/Settings/Connection
                 Connection connection = dbusConn.getRemoteObject("org.freedesktop.NetworkManager",
                         connectionIf.getObjectPath().toString(), Connection.class);
 
+                // Call /org/freedesktop/NetworkManager/Settings/Connection/GetSettings
                 Map<String, Map<String, Variant<?>>> connectionSettings = connection.GetSettings();
 
-                System.out.println("Id: " + connectionSettings.get("connection").get("id"));
-                System.out.println("Uuid: " + connectionSettings.get("connection").get("uuid"));
-                System.out.println("Iface: " + connectionSettings.get("connection").get("interface-name"));
+                System.out.println("Connection id: " + connectionSettings.get("connection").get("id"));
 
-                // Deep copy
-                Map<String, Variant<?>> connectionMap = new HashMap<>();
-                for (String key : connectionSettings.get("connection").keySet()) {
-                    connectionMap.put(key, connectionSettings.get("connection").get(key));
-                }
-                Map<String, Variant<?>> ipv4Map = new HashMap<>();
-                for (String key : connectionSettings.get("ipv4").keySet()) {
-                    ipv4Map.put(key, connectionSettings.get("ipv4").get(key));
-                }
-
-                // Update DNS with 8.8.8.8
-                UInt32[] newDNS = { new UInt32(0x08080808) };
-                ipv4Map.put("dns", new Variant<UInt32[]>(newDNS));
-
-                Map<String, Map<String, Variant<?>>> newConnectionSettings = new HashMap<>();
-                newConnectionSettings.put("ipv4", ipv4Map);
-                newConnectionSettings.put("connection", connectionMap);
-
-                System.out.println("DNS: " + newConnectionSettings.get("ipv4").get("dns"));
-
-                // Update & save connection settings
-                connection.Update(newConnectionSettings);
-                connection.Save();
+                // Critical stuff
+                System.out.println("Address-data Settings.Connection[ipv4][address-data]");
+                System.out.print("    ");
+                System.out.println(connectionSettings.get("ipv4").get("address-data"));
+                // Will output [[{ address => [192.168.1.223],prefix => [24] }]]
             }
 
         } catch (IOException _ex) {
             _ex.printStackTrace();
         }
-        System.out.println( "Hello World!" );
     }
 }
