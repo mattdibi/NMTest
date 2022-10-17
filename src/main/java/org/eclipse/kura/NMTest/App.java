dcea16af9ee503ff2aff508ed8feb8bc5b8a273c
Mattia Dal Ben <matthewdibi@gmail.com>
2022-10-17 11:06:40 +0200

    feat: update DNS

--- src/main/java/org/eclipse/kura/NMTest/App.java
+++ src/main/java/org/eclipse/kura/NMTest/App.java
@@ -1,11 +1,15 @@
 package org.eclipse.kura.NMTest;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.freedesktop.dbus.connections.impl.DBusConnection;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.freedesktop.dbus.interfaces.DBusInterface;
+import org.freedesktop.dbus.types.UInt32;
 import org.freedesktop.dbus.types.Variant;
 
 import org.freedesktop.networkmanager.Settings;
@@ -29,11 +33,29 @@ public class App {
 
                 System.out.println("Connection id: " + connectionSettings.get("connection").get("id"));
 
-                // Critical stuff
-                System.out.println("Address-data Settings.Connection[ipv4][address-data]");
-                System.out.print("    ");
-                System.out.println(connectionSettings.get("ipv4").get("address-data"));
-                // Will output [[{ address => [192.168.1.223],prefix => [24] }]]
+                // Deep copy
+                Map<String, Variant<?>> connectionMap = new HashMap<>();
+                for (String key : connectionSettings.get("connection").keySet()) {
+                    connectionMap.put(key, connectionSettings.get("connection").get(key));
+                }
+                Map<String, Variant<?>> ipv4Map = new HashMap<>();
+                for (String key : connectionSettings.get("ipv4").keySet()) {
+                    ipv4Map.put(key, connectionSettings.get("ipv4").get(key));
+                }
+
+                // Update DNS with 8.8.8.8
+                UInt32[] newDNS = { new UInt32(0x08080808) };
+                ipv4Map.put("dns", new Variant<UInt32[]>(newDNS));
+
+                Map<String, Map<String, Variant<?>>> newConnectionSettings = new HashMap<>();
+                newConnectionSettings.put("ipv4", ipv4Map);
+                newConnectionSettings.put("connection", connectionMap);
+
+                System.out.println("DNS: " + newConnectionSettings.get("ipv4").get("dns"));
+
+                // Update & save connection settings
+                connection.Update(newConnectionSettings);
+                connection.Save();
             }
 
         } catch (IOException _ex) {
