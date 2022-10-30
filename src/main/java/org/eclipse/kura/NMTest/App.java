6deb83a42338a9385235ddd933c0fbf6f8550490
Mattia Dal Ben <matthewdibi@gmail.com>
2022-10-30 16:50:25 +0100

    feat: add update-ip4-method example

--- src/main/java/org/eclipse/kura/NMTest/App.java
+++ src/main/java/org/eclipse/kura/NMTest/App.java
@@ -14,23 +14,48 @@ import org.freedesktop.dbus.types.Variant;
 import org.freedesktop.networkmanager.Settings;
 import org.freedesktop.networkmanager.settings.Connection;
 
+/*
+    This example updates a connection's IPv4 method with the Update() method.
+
+    This uses the new NM 1.0 setting properties.
+
+    Configuration settings are described at
+    https://networkmanager.dev/docs/api/latest/ref-settings.html
+*/
+
 public class App {
     public static void main( String[] args ) throws DBusException {
+        if(args.length < 3) {
+            System.out.println("Usage: <uuid> <auto|static> [address prefix gateway]");
+            System.exit(1);
+        }
+
+        String method = args[2];
+        if( method == "static" && args.length < 5) {
+            System.out.println("Usage: <uuid> <static> address prefix [gateway]");
+            System.exit(1);
+        }
+
+        // Convert method to NM method
+        if(method == "static") {
+            method = "manual";
+        }
+
         try (DBusConnection dbusConn = DBusConnection.getConnection(DBusConnection.DEFAULT_SYSTEM_BUS_ADDRESS)) {
-            // Get /org/freedesktop/NetworkManager/Settings
+
             Settings settings = dbusConn.getRemoteObject("org.freedesktop.NetworkManager",
                     "/org/freedesktop/NetworkManager/Settings", Settings.class);
 
-            // Iterate over connections
             for (DBusInterface connectionIf : settings.ListConnections()) {
-                // Get /org/freedesktop/NetworkManager/Settings/Connection
                 Connection connection = dbusConn.getRemoteObject("org.freedesktop.NetworkManager",
                         connectionIf.getObjectPath().toString(), Connection.class);
 
-                // Call /org/freedesktop/NetworkManager/Settings/Connection/GetSettings
                 Map<String, Map<String, Variant<?>>> connectionSettings = connection.GetSettings();
 
-                System.out.println("Connection id: " + connectionSettings.get("connection").get("id"));
+                // Look for the requested connection UUID
+                if(connectionSettings.get("connection").get("uuid").toString() != args[1]) {
+                    continue;
+                }
 
                 // Deep copy
                 Map<String, Variant<?>> connectionMap = new HashMap<>();
@@ -41,22 +66,29 @@ public class App {
                 for (String key : connectionSettings.get("ipv4").keySet()) {
                     ipv4Map.put(key, connectionSettings.get("ipv4").get(key));
                 }
-                
+
                 // Clear existing address info
                 ipv4Map.remove("addresses");
                 ipv4Map.remove("address-data");
                 ipv4Map.remove("gateway");
 
-                // Update IP address
-                Map<String, Variant<?>> address = new HashMap<>();
-                address.put("address", new Variant<String>("192.168.1.224"));
-                address.put("prefix", new Variant<UInt32>(new UInt32(24)));
+                // Set the method and change properties
+                ipv4Map.put("method", new Variant<String>(method));
 
-                List<Map<String, Variant<?>>> addressData = Arrays.asList(address);
-                ipv4Map.put("address-data", new Variant<>(addressData, "aa{sv}"));
-                ipv4Map.put("gateway", new Variant<String>("192.168.1.1"));
-                ipv4Map.put("method", new Variant<String>("manual"));
-                
+                if(method == "manual") {
+                    // Add the static IP address, prefix and (optional) gateway
+                    Map<String, Variant<?>> address = new HashMap<>();
+                    address.put("address", new Variant<String>(args[3]));
+                    address.put("prefix", new Variant<UInt32>(new UInt32(args[4])));
+
+                    List<Map<String, Variant<?>>> addressData = Arrays.asList(address);
+                    ipv4Map.put("address-data", new Variant<>(addressData, "aa{sv}"));
+
+                    if(args.length == 6) {
+                        ipv4Map.put("gateway", new Variant<String>(args[5]));
+                    }
+                }
+
                 // Build output data
                 Map<String, Map<String, Variant<?>>> newConnectionSettings = new HashMap<>();
                 newConnectionSettings.put("ipv4", ipv4Map);
